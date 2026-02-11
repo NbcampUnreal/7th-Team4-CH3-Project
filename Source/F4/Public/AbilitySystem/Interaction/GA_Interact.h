830f56e82c1809be30c948ba5bf0c1236a7c2e31
minsung <flymin00712@gmail.com>
2026-02-11 19:38:55 +0900

    Feat : 캐릭터 상호작용

---
+++ Source/F4/Public/AbilitySystem/Interaction/GA_Interact.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Interact.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class F4_API UGA_Interact : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UGA_Interact(); 
+	
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+	
+protected:
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Interact | Trace")
+	float InteractionDistance = 500.f; 
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Interact | Trace")
+	float InteractionRadius = 50.f; 
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Interact | Trace")
+	float DrawTime = 1.f;  
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Interact | Trace")
+	bool bDrawTrace = true; 
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Interact | Trace")
+	FColor TraceColor = FColor::Yellow;
+};
