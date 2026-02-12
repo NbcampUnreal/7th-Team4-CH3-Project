213413e0004e166101d734ca7934f23c6c07f9c4
dooyeon kang <dyhouse0517@gmail.com>
2026-02-12 16:25:23 +0900

    feat: reload game ability 추가

---
+++ Source/F4/Public/AbilitySystem/Abilities/Combat/GA_Reload.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Reload.generated.h"
+
+UCLASS()
+class F4_API UGA_Reload : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UGA_Reload();
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Reload")
+	TObjectPtr<UAnimMontage> ReloadMontage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Reload")
+	TSubclassOf<UGameplayEffect> ReloadEffectClass;
+
+	UFUNCTION()
+	void OnMontageCompleted();
+
+	UFUNCTION()
+	void OnMontageCancelled();
+
+	UFUNCTION()
+	void OnReloadGameplayEvent(FGameplayEventData EventData);
+
+	void PerformReload();
+};
