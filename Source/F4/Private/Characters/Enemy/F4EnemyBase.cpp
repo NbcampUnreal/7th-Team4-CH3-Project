330cb90e6f6e1ea3f1a03dbac30fd210c68490bf
minsung <flymin00712@gmail.com>
2026-02-13 17:13:22 +0900

    fix : 버그 수정

--- Source/F4/Private/Characters/Enemy/F4EnemyBase.cpp
+++ Source/F4/Private/Characters/Enemy/F4EnemyBase.cpp
@@ -67,7 +67,7 @@ void AF4EnemyBase::InitEnemyStats()
 		ASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
 	}
 	
-	OnSpeedAttributeChanged();
+	// OnSpeedAttributeChanged(); -> BaseCharacter에서 처리해줘요 호출 필요 x 
 }
 
 bool AF4EnemyBase::IsInAttackRange(AActor* TargetActor)
