53020bfd8f019a21a5071c7ce219206c1f013639
minsung <flymin00712@gmail.com>
2026-02-19 20:10:06 +0900

    Feat : Rifle AnimInst 추가

---
+++ Source/F4/Public/Animation/AnimInst/F4RifleAnimInst.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInst/F4BaseAnimInst.h"
+#include "F4RifleAnimInst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class F4_API UF4RifleAnimInst : public UF4BaseAnimInst
+{
+	GENERATED_BODY()
+	
+};
