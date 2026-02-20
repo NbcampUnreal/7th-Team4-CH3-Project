78ff136ba855bb5473a5b71752e1adac241325f3
Park WonJong <kidsmon@naver.com>
2026-02-20 17:21:57 +0900

    feat: F4ItemDataAsset 생성

---
+++ Source/F4/Public/Items/F4ItemDataAsset.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "F4ItemDataAsset.generated.h"
+
+UENUM(BlueprintType)
+enum class EF4ItemType : uint8
+{
+	Weapon		UMETA(DisplayName = "Weapon"),
+	Consumable	UMETA(DisplayName = "Consumable")
+};
+
+UCLASS(Abstract)
+class F4_API UF4ItemDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item|Info")
+	FText DisplayName;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item|Info")
+	EF4ItemType ItemType;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item|Visual")
+	TObjectPtr<UStaticMesh> PickupMesh;;
+};
