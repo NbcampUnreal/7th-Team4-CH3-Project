c83ae5f65177f0d068bd4e0f18d2c53df06c7b61
Park WonJong <kidsmon@naver.com>
2026-02-20 17:26:05 +0900

    refactor: 부모 클래스 추가로 중복 코드 제거

--- Source/F4/Private/Items/Weapons/F4WeaponDataAsset.cpp
+++ Source/F4/Private/Items/Weapons/F4WeaponDataAsset.cpp
@@ -1,5 +1,10 @@
 #include "Items/Weapons/F4WeaponDataAsset.h"
 
+UF4WeaponDataAsset::UF4WeaponDataAsset()
+{
+	ItemType = EF4ItemType::Weapon;
+}
+
 FPrimaryAssetId UF4WeaponDataAsset::GetPrimaryAssetId() const
 {
 	return FPrimaryAssetId("Weapon", GetFName());
