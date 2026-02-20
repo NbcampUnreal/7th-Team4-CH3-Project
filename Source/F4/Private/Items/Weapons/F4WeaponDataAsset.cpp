bf6d4f7829f455efb63b5ec36858706f26ff8a1d
Park WonJong <kidsmon@naver.com>
2026-02-20 17:22:41 +0900

    chore: 주석삭제

--- Source/F4/Private/Items/Weapons/F4WeaponDataAsset.cpp
+++ Source/F4/Private/Items/Weapons/F4WeaponDataAsset.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Items/Weapons/F4WeaponDataAsset.h"
 
 FPrimaryAssetId UF4WeaponDataAsset::GetPrimaryAssetId() const
