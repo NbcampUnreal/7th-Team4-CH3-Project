3f29e77d6e98c0ce0ca66705f4798e0de9cdf597
minsung <flymin00712@gmail.com>
2026-02-23 17:00:25 +0900

    feat : 크로스헤어 추가

---
+++ Source/F4/Public/UI/CrosshairWidget.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CrosshairWidget.generated.h"
+
+class UImage;
+
+
+UCLASS()
+class F4_API UCrosshairWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UFUNCTION(BlueprintCallable, Category = "UI|Crosshair")
+	void UpdateCrosshair(const float Value); 
+	
+	UFUNCTION(BlueprintCallable, Category = "UI|Crosshair")
+	void ToggleCrosshair(bool bDraw);
+	
+	UFUNCTION(BlueprintCallable, Category = "UI|Crosshair")
+	void ToggleDot(bool bDraw);
+	
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	UImage* Dot; 
+	
+	UPROPERTY(meta = (BindWidget))
+	UImage* LineTop;
+	
+	UPROPERTY(meta = (BindWidget))
+	UImage* LineBottom;
+	
+	UPROPERTY(meta = (BindWidget))
+	UImage* LineLeft;
+	
+	UPROPERTY(meta = (BindWidget))	
+	UImage* LineRight;
+};
