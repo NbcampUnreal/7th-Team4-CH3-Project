f5fd2a2efaeaa29f097c07af456d53886e530a60
Park WonJong <kidsmon@naver.com>
2026-02-25 11:53:03 +0900

    refactor: 총알 및 포션류에 필요한 조각 파일 추가

---
+++ Source/F4/Private/Items/ConsumableItems/F4ItemFragment_Consumable.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/ConsumableItems/F4ItemFragment_Consumable.h"
+
---
+++ Source/F4/Public/Items/ConsumableItems/F4ItemFragment_Consumable.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inventory/F4ItemFragment.h"
+#include "F4ItemFragment_Consumable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class F4_API UF4ItemFragment_Consumable : public UF4ItemFragment
+{
+	GENERATED_BODY()
+	
+};
