6df6dea903fb0056a02a47a63b4ceb1f678cd07d
minsung <flymin00712@gmail.com>
2026-02-27 21:06:37 +0900

    chore : 주석제거

--- Source/F4/Private/UI/MainMenuWidget.cpp
+++ Source/F4/Private/UI/MainMenuWidget.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UI/MainMenuWidget.h"
 #include "Components/Button.h"
 #include "Components/PanelWidget.h"
