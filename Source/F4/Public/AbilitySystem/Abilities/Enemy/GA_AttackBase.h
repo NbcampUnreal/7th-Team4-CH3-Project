f161b0e6acfe46092baca0c1ae410c5c518f26cb
Kim Taewoo <xodn246@gmail.com>
2026-02-28 14:59:18 +0900

    fix: 안쓰는 변수 제거

--- Source/F4/Public/AbilitySystem/Abilities/Enemy/GA_AttackBase.h
+++ Source/F4/Public/AbilitySystem/Abilities/Enemy/GA_AttackBase.h
@@ -16,10 +16,6 @@ protected:
 	TObjectPtr<UAnimMontage> AttackMontage;
 	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Attack | Montage")
 	float MontageSpeed = 1.f;
-	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Attack | Montage")
-	float ExtraRootMotionForce = 800.f;
-	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="Attack | Montage")
-	float ExtraRootMotionDuration = 0.4f;
 
 	UFUNCTION()
 	virtual void OnMontageCompleted();
