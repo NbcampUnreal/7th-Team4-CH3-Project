7d231515bb4844a61a803ed64a5fabd433eb4a67
Park WonJong <kidsmon@naver.com>
2026-03-02 19:59:43 +0900

    fix: 개행 오타 수정

--- Source/F4/Public/Inventory/F4ItemFragment_Spawnable.h
+++ Source/F4/Public/Inventory/F4ItemFragment_Spawnable.h
@@ -13,8 +13,9 @@ class F4_API UF4ItemFragment_Spawnable : public UF4ItemFragment
 
 public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Pickup|Class")
-	TSubclassOf<AF4PickupActor> PickupActorClass;	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spawning")
-
+	TSubclassOf<AF4PickupActor> PickupActorClass;	
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spawning")
 	int32 MinSpawnQuantity = 1;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spawning")
