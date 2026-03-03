1d5f6635ace52816a7fae2198e7442880e53ef02
minsung <flymin00712@gmail.com>
2026-03-03 22:02:30 +0900

    feat : 로비 브금 재생 추가

--- Source/F4/Private/System/LobbyGameMode.cpp
+++ Source/F4/Private/System/LobbyGameMode.cpp
@@ -1,6 +1,7 @@
 #include "System/LobbyGameMode.h"
 
 #include "Kismet/GameplayStatics.h"
+#include "System/F4GameInstance.h"
 #include "System/F4GameState.h"
 
 ALobbyGameMode::ALobbyGameMode()
@@ -11,11 +12,25 @@ ALobbyGameMode::ALobbyGameMode()
 void ALobbyGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Warning, TEXT("Current GameMode is Lobby GameMode."));
 
 	if (AF4GameState* GS = GetGameState<AF4GameState>())
 	{
 		GS->SetTimeFlows(false);
 	}
+	
+	UF4GameInstance* GI = Cast<UF4GameInstance>(GetGameInstance()); 
+	if (GI)
+	{
+		if (LobbyBGM)
+		{
+			GI->PlayBGM(LobbyBGM, bFade);
+			UE_LOG(LogTemp, Warning, TEXT("Lobby BGM Started"));
+		}
+	}
+	
+	
 }
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
