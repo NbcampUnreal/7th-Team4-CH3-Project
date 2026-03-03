58230b62d8c45a7dec89ee07f913b32a5d1d4fc3
minsung <flymin00712@gmail.com>
2026-03-03 16:28:03 +0900

    Task: 레벨 이동 액터 배치

--- Source/F4/Private/Enviroment/LevelMoveActor.cpp
+++ Source/F4/Private/Enviroment/LevelMoveActor.cpp
@@ -50,7 +50,7 @@ void ALevelMoveActor::BeginPlay()
 }
 
 void ALevelMoveActor::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                                           UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	APawn* OtherPawn = Cast<APawn>(OtherActor);
 	if (!OtherPawn || !OtherPawn->IsPlayerControlled())
