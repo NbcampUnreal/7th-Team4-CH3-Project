65de3632f68bd9a91941469d5e6f51436a066d98
minsung <flymin00712@gmail.com>
2026-03-03 16:16:07 +0900

    feat : 레벨 이동 액터 방어 코드 추가

--- Source/F4/Private/Enviroment/LevelMoveActor.cpp
+++ Source/F4/Private/Enviroment/LevelMoveActor.cpp
@@ -52,12 +52,24 @@ void ALevelMoveActor::BeginPlay()
 void ALevelMoveActor::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                            UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	APawn* OtherPawn = Cast<APawn>(OtherActor);
+	if (!OtherPawn || !OtherPawn->IsPlayerControlled())
+	{
+		return;
+	}
+	
 	WidgetComponent->SetVisibility(true);
 }
 
 void ALevelMoveActor::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
+	APawn* OtherPawn = Cast<APawn>(OtherActor);
+	if (!OtherPawn || !OtherPawn->IsPlayerControlled())
+	{
+		return;
+	}
+	
 	WidgetComponent->SetVisibility(false);
 }
 
